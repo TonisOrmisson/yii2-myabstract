0aea3fc6ab14daaf82bf7cea90e148e54f1e0646
Tõnis Ormisson <tonis@andmemasin.eu>
2018-05-21 12:06:02 +0300

    DEV: add BaseAdminController

---
+++ controllers/BaseAdminController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace andmemasin\myabstract\controllers;
+
+
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+class BaseAdminController extends Controller
+{
+
+    /** @var  string name of admin permission */
+    public $adminPermission;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => [$this->adminPermission],
+                    ],
+                ],
+            ],
+
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+}
