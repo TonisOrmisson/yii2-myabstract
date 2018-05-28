7ff2d9f794b074ad74f819eb89452d72bd3b920a
Tõnis Ormisson <tonis@andmemasin.eu>
2018-05-28 12:02:56 +0300

    implement StatusHistory widget

--- User.php
+++ User.php
@@ -5,6 +5,13 @@ namespace andmemasin\myabstract;
 use yii\db\ActiveRecord;
 use andmemasin\myabstract\traits\MyActiveTrait;
 
+/**
+ * Class User
+ * @property string $username
+ *
+ * @package andmemasin\myabstract
+ * @author Tõnis Ormisson <tonis@andmemasin.eu>
+ */
 class User extends ActiveRecord
 {
 
