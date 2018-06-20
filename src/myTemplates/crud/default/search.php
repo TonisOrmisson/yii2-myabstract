1fab5926e6c0654cdbc5c9266603afa0e06a4a82
Tõnis Ormisson <tonis@andmemasin.eu>
2018-06-20 11:52:48 +0300

    fix searchmodel    return (new Model)->scenarios();

--- src/myTemplates/crud/default/search.php
+++ src/myTemplates/crud/default/search.php
@@ -51,7 +51,7 @@ class <?= $searchModelClass ?> extends <?= isset($modelAlias) ? $modelAlias : $m
     public function scenarios()
     {
         // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
+        return (new Model)->scenarios();
     }
 
     /**
