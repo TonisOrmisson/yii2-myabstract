2d938383794ad12c2beb4f25e0da06f10962203a
Tõnis Ormisson <tonis@andmemasin.eu>
2018-07-26 15:59:53 +0300

    model properies also are allowed in

--- src/test/ModelTestTrait.php
+++ src/test/ModelTestTrait.php
@@ -16,20 +16,20 @@ trait ModelTestTrait
     public function testAttributeLabelsForExistingAttributesOnly() {
         // labels only for actually existing attributes
         foreach ($this->model->attributeLabels() as $key => $label) {
-            $this->assertArrayHasKey($key, $this->model->attributes);
+            $this->assertArrayHasKey($key, array_merge(get_object_vars($this->model), $this->model->attributes));
         }
     }
     public function testAttributeHintsForExistingAttributesOnly() {
         // labels only for actually existing attributes
         foreach ($this->model->attributeHints() as $key => $label) {
-            $this->assertArrayHasKey($key, $this->model->attributes);
+            $this->assertArrayHasKey($key, array_merge(get_object_vars($this->model), $this->model->attributes));
         }
     }
     public function testRulesForExistingAttributesOnly() {
         // labels only for actually existing attributes
         foreach ($this->model->rules() as $rule) {
             foreach ($rule[0] as $attribute) {
-                $this->assertArrayHasKey($attribute, $this->model->attributes);
+                $this->assertArrayHasKey($attribute, array_merge(get_object_vars($this->model), $this->model->attributes));
             }
         }
     }
