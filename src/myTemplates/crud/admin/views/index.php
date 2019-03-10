899af020b5ad39a6edee0ed6979cc2e131fea3db
Tõnis Ormisson <tonis@andmemasin.eu>
2019-03-10 15:24:38 +0200

    add admin template

---
+++ src/myTemplates/crud/admin/views/index.php
@@ -0,0 +1,86 @@
+<?php
+
+use yii\helpers\Inflector;
+use yii\helpers\StringHelper;
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\crud\Generator */
+
+$urlParams = $generator->generateUrlParams();
+$nameAttribute = $generator->getNameAttribute();
+
+echo "<?php\n";
+?>
+
+use yii\helpers\Html;
+use <?= $generator->indexWidgetType === 'grid' ? "yii\\grid\\GridView" : "yii\\widgets\\ListView" ?>;
+use <?= ltrim($generator->modelClass, '\\') ?>;
+
+/* @var $this yii\web\View */
+<?= !empty($generator->searchModelClass) ? "/* @var \$searchModel " . ltrim($generator->searchModelClass, '\\') . " */\n" : '' ?>
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>;
+$this->params['breadcrumbs'][] = $this->title;
+
+// DO NOT REMOVE This is for automated testing to validate we see that page
+echo '<div id="action-'.Yii::$app->controller->getUniqueId()."/".Yii::$app->controller->action->id.'" style="visibility:hidden"></div>';
+?>
+<div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-index">
+
+    <div class="box box-default">
+        <div class="box-body">
+            <?php if (!empty($generator->searchModelClass)): ?>
+                <?= "    <?php " . ($generator->indexWidgetType === 'grid' ? "// " : "") ?>echo $this->render('_search', ['model' => $searchModel]); ?>
+            <?php endif; ?>
+
+            <p>
+                <?= "<?= " ?>Html::a(<?= $generator->generateString('Create ' . Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>, ['create'], ['class' => 'btn btn-success']) ?>
+            </p>
+
+            <?php if ($generator->indexWidgetType === 'grid'): ?>
+                <?= "<?= " ?>GridView::widget([
+                'dataProvider' => $dataProvider,
+                <?= !empty($generator->searchModelClass) ? "'filterModel' => \$searchModel,\n        'columns' => [\n" : "'columns' => [\n"; ?>
+                ['class' => 'yii\grid\SerialColumn'],
+
+                <?php
+                $count = 0;
+                if (($tableSchema = $generator->getTableSchema()) === false) {
+                    foreach ($generator->getColumnNames() as $name) {
+                        if (++$count < 6) {
+                            echo "            '" . $name . "',\n";
+                        } else {
+                            echo "            // '" . $name . "',\n";
+                        }
+                    }
+                } else {
+                    foreach ($tableSchema->columns as $column) {
+                        $format = $generator->generateColumnFormat($column);
+                        if (++$count < 6) {
+                            echo "            '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+                        } else {
+                            echo "            // '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+                        }
+                    }
+                }
+                ?>
+
+                ['class' => 'yii\grid\ActionColumn'],
+                ],
+                ]); ?>
+            <?php else: ?>
+                <?= "<?= " ?>ListView::widget([
+                'dataProvider' => $dataProvider,
+                'itemOptions' => ['class' => 'item'],
+                'itemView' => function ($model, $key, $index, $widget) {
+                return Html::a(Html::encode($model-><?= $nameAttribute ?>), ['view', <?= $urlParams ?>]);
+                },
+                ]) ?>
+            <?php endif; ?>
+
+        </div>
+    </div>
+
+
+</div>
