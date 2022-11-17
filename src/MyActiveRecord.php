3db3c0f1f950ffd43d6c0cfe619741af3644239e
tonis ormisson <tonis@andmemasin.eu>
2022-11-17 14:05:01 +0200

    new api Rating/can-rate

--- src/MyActiveRecord.php
+++ src/MyActiveRecord.php
@@ -4,7 +4,6 @@ namespace andmemasin\myabstract;
 
 use andmemasin\myabstract\interfaces\UserInterface;
 use andmemasin\myabstract\traits\ConsoleAwareTrait;
-use andmemasin\surveyapp\traits\ApplicationAwareTrait;
 use Yii;
 use andmemasin\myabstract\traits\ModuleTrait;
 use andmemasin\myabstract\traits\MyActiveTrait;
@@ -26,7 +25,6 @@ class MyActiveRecord extends ActiveRecord
     use MyActiveTrait;
     use ModuleTrait;
     use ConsoleAwareTrait;
-    use ApplicationAwareTrait;
 
     public function getUserCreated() : UserInterface
     {
