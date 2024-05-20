5fdd56388773246d2c3ba8a11c3396215f28959d
tonis <tonis@andmemasin.eu>
2024-05-20 10:49:14 +0300

    remove all Settings related things except SettingInterface, prepare v8.0.0

--- src/interfaces/SettingInterface.php
+++ src/interfaces/SettingInterface.php
@@ -9,13 +9,33 @@ namespace andmemasin\myabstract\interfaces;
  */
 interface SettingInterface
 {
-
-    public function findOneByKey(string $key) : ?static;
+    public function findOneByKey(string $key) : ?SettingInterface;
     public function getKey() : mixed;
     public function getValue() : mixed;
     public function setKey(mixed $key) : void;
     public function setValue(mixed $value) : void;
-    public function save() : bool;
+    public function createNew(string $key, mixed $value, array $data = []) : SettingInterface;
+    public function valueAsArray() : array;
+
+    /**
+     * @param null|string[] $attributeNames
+     * @param bool $clearErrors
+     * @return bool
+     */
+    public function validate($attributeNames = null, $clearErrors = true);
+
+    /**
+     * @return SettingInterface[]
+     */
+    public function getSettings() : array;
+    public function settingByIndex(string|int $index) : ?SettingInterface;
+
+    /**
+     * @param bool $runValidation
+     * @param ?array $attributeNames
+     * @return bool
+     */
+    public function save($runValidation = true, $attributeNames = null);
 
     /**
      * @return array<mixed>
