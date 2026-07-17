494bc6fd40a1d169d9b1c3480b42e30880c0b519
Tõnis Ormisson <tonis@andmemasin.eu>
2026-07-17 12:52:37 +0300

    Support root dependencies in MyAbstract tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -2,6 +2,40 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-require_once(__DIR__ . '/../vendor/autoload.php');
-require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+$autoloadPaths = [
+    dirname(__DIR__, 4) . '/vendor/autoload.php',
+    __DIR__ . '/../vendor/autoload.php',
+];
 
+$autoloadFile = null;
+foreach ($autoloadPaths as $candidate) {
+    if (is_file($candidate)) {
+        $autoloadFile = $candidate;
+        break;
+    }
+}
+
+if ($autoloadFile === null) {
+    throw new RuntimeException('Unable to locate Composer autoload.php for MyAbstract tests.');
+}
+
+require_once($autoloadFile);
+
+$yiiPaths = [
+    dirname(__DIR__, 4) . '/vendor/yiisoft/yii2/Yii.php',
+    __DIR__ . '/../vendor/yiisoft/yii2/Yii.php',
+];
+
+$yiiFile = null;
+foreach ($yiiPaths as $candidate) {
+    if (is_file($candidate)) {
+        $yiiFile = $candidate;
+        break;
+    }
+}
+
+if ($yiiFile === null) {
+    throw new RuntimeException('Unable to locate Yii.php for MyAbstract tests.');
+}
+
+require_once($yiiFile);
