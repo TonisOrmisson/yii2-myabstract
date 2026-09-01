0524239386c73170eab5e7efe68b16368af514f5
Tõnis Ormisson <tonis@andmemasin.eu>
2026-09-01 07:43:01 +0300

    Cover Yii array indexing replacement

--- tests/unit/StaticModelTest.php
+++ tests/unit/StaticModelTest.php
@@ -2,6 +2,8 @@
 namespace andmemasin\myabstract;
 
 use Codeception\Stub;
+use yii\base\DynamicModel;
+use yii\helpers\ArrayHelper;
 
 class StaticModelTest extends \Codeception\Test\Unit
 {
@@ -35,5 +37,39 @@ class StaticModelTest extends \Codeception\Test\Unit
     public function testGetByKeyNoModels()
     {
         $this->assertNull($this->model::getByKey("there-is-nothing"));
+    }
+
+    public function testGetByKeyUsesLastDuplicateValue(): void
+    {
+        $model = IndexedStaticModel::getByKey('duplicate');
+
+        $this->assertInstanceOf(IndexedStaticModel::class, $model);
+        $this->assertSame('last', $model->value);
+    }
+
+    public function testYiiArrayIndexPreservesIndexByColumnBehavior(): void
+    {
+        $model = new DynamicModel(['key' => 'duplicate', 'value' => 'model']);
+        $array = ['key' => 7, 'value' => 'array'];
+
+        $this->assertSame(
+            [7 => $array, 'duplicate' => $model],
+            ArrayHelper::index([$array, ['key' => 'duplicate', 'value' => 'first'], $model], 'key')
+        );
+        $this->assertSame([], ArrayHelper::index([], 'key'));
+    }
+}
+
+final class IndexedStaticModel extends StaticModel
+{
+    public string $key = '';
+    public string $value = '';
+
+    public function getModelAttributes(): array
+    {
+        return [
+            ['key' => 'duplicate', 'value' => 'first'],
+            ['key' => 'duplicate', 'value' => 'last'],
+        ];
     }
 }
